5045432c4a4143079f63911450da8215892be03f
Ronald Veth <r.veth@betabit.nl>
2021-04-20 08:40:26 +0200

    Now possible to add columnSelector to FindTableElement extension

---
+++ Selenium.TableElement.UiTests/PageObjects/DivTable.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.IO;
+
+namespace Selenium.TableElement.UiTests.PageObjects
+{
+    public class DivTable
+    {
+        private readonly IWebDriver _webDriver;
+
+        public ITableElement DivTableElement => _webDriver.FindTableElement(By.ClassName("divTableHead"), By.CssSelector(".divTableBody>.divTableRow"), By.ClassName("divTableCell"));
+
+        public DivTable(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        public void Open()
+        {
+            _webDriver.Navigate().GoToUrl(new Uri(Path.Combine(Path.GetDirectoryName(GetType().Assembly.Location), "DemoHtml/divtable.html")).AbsoluteUri);
+        }
+    }
+}
