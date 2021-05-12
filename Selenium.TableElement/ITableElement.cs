6347ccd90211d3ea00af80247bacf729588a9e3d
Ronald Veth <r.veth@betabit.nl>
2021-05-12 16:16:34 +0200

    Able now to create only tablerowelement by giving self found header values. Also added documentation

--- Selenium.TableElement/ITableElement.cs
+++ Selenium.TableElement/ITableElement.cs
@@ -3,9 +3,18 @@ using System.Collections.ObjectModel;
 
 namespace Selenium.TableElement
 {
+    /// <summary>
+    /// Defines the interface through which the user can access table header values and table rows.
+    /// </summary>
     public interface ITableElement
     {
+        /// <summary>
+        /// All found headers, handy to verify if headerSelector/headerElements is correct
+        /// </summary>
         IEnumerable<string> TableHeaderValues { get; }
+        /// <summary>
+        /// All TableRowElements found by rowSelector/rowElements
+        /// </summary>
         ReadOnlyCollection<ITableRowElement> TableRowElements { get; }
     }
 }
--- Selenium.TableElement/TableRowElement.cs
+++ Selenium.TableElement/TableRowElement.cs
@@ -7,13 +7,25 @@ using System.Linq;
 
 namespace Selenium.TableElement
 {
+    /// <summary>
+    /// TableRowElement supports testing Html TableRows with Selenium.
+    /// </summary>
     public class TableRowElement : IWrapsElement, ITableRowElement
     {
         private readonly ReadOnlyCollection<IWebElement> _columns;
         private readonly IDictionary<string, int> _headerIndexer;
 
+        /// <summary>
+        /// Gets the <see cref="IWebElement"/> wrapped by this object.
+        /// </summary>
         public IWebElement WrappedElement { get; }
 
+        /// <summary>
+        /// Creates TableRowElement
+        /// </summary>
+        /// <param name="headerIndexer">Indexer that contains header values</param>
+        /// <param name="rowElement">IWebelement that represents row</param>
+        /// <param name="rowColumnSelector">Selector to find IWebElements inside a row representing the columns</param>
         public TableRowElement(IDictionary<string, int> headerIndexer, IWebElement rowElement, By rowColumnSelector)
         {
             _columns = ColumnsIncludingColspan(rowElement.FindElements(rowColumnSelector));
@@ -21,6 +33,24 @@ namespace Selenium.TableElement
             WrappedElement = rowElement;
         }
 
+        /// <summary>
+        /// Creates TableRowElement
+        /// </summary>
+        /// <param name="headerElements">Collection of IWebelements that represents headerColumns</param>
+        /// <param name="rowElement">IWebelement that represents row</param>
+        /// <param name="rowColumnSelector">Selector to find IWebElements inside a row representing the columns</param>
+        public TableRowElement(IEnumerable<IWebElement> headerElements, IWebElement rowElement, By rowColumnSelector)
+        {
+            _columns = ColumnsIncludingColspan(rowElement.FindElements(rowColumnSelector));
+            _headerIndexer = new HeaderIndexer().HeadersIncludingColspanAndDuplicate(headerElements);
+            WrappedElement = rowElement;
+        }
+
+        /// <summary>
+        /// Get a column of the tablerow.
+        /// </summary>
+        /// <param name="nameHeader">Header value to get corresponding column.</param>
+        /// <returns>The matching <see cref="IWebElement"/> on the current context.</returns>
         public IWebElement GetColumn(string nameHeader)
         {
             if (!_headerIndexer.TryGetValue(nameHeader, out int result))
@@ -31,6 +61,11 @@ namespace Selenium.TableElement
             return _columns[result];
         }
 
+        /// <summary>
+        /// Get a column of the tablerow.
+        /// </summary>
+        /// <param name="index">The zero-based index of the element to get.</param>
+        /// <returns>The matching <see cref="IWebElement"/> on the current context.</returns>
         public IWebElement GetColumn(int index)
         {
             return _columns[index];
